784df7d0c60395e4f1365aaf2ebe27f2ac580845
Defense-geek <mrchowdary1700@gmail.com>
2026-05-03 03:36:16 +0530

    filter

--- .buildozer/android/platform/build-arm64-v8a/dists/smartibassist/obj/local/arm64-v8a/objs-debug/webp/src/enc/config_enc.o.d
+++
@@ -1,8 +0,0 @@
-/home/mm/Documents/hackathon/smart_ib_assist/.buildozer/android/platform/build-arm64-v8a/build/bootstrap_builds/sdl2/obj/local/arm64-v8a/objs-debug/webp/src/enc/config_enc.o: \
-  /home/mm/Documents/hackathon/smart_ib_assist/.buildozer/android/platform/build-arm64-v8a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/enc/config_enc.c \
-  /home/mm/Documents/hackathon/smart_ib_assist/.buildozer/android/platform/build-arm64-v8a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/encode.h \
-  /home/mm/Documents/hackathon/smart_ib_assist/.buildozer/android/platform/build-arm64-v8a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/./types.h
-
-/home/mm/Documents/hackathon/smart_ib_assist/.buildozer/android/platform/build-arm64-v8a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/encode.h:
-
-/home/mm/Documents/hackathon/smart_ib_assist/.buildozer/android/platform/build-arm64-v8a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/./types.h:
